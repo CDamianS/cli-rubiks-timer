6698ed8db8f7a3d00d7d83a3c0123af404ce23c5
CDamianS <A01656277@tec.mx>
2022-12-31 13:01:24 -0600

    Added template stopwatch

--- main.go
+++ main.go
@@ -1,39 +1,103 @@
 package main
 
+// TODO: stop the timer before a keypress
 import (
 	"fmt"
-	// "github.com/charmbracelet/bubbles/list"
-	// tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"os"
+	"time"
+
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/stopwatch"
+	tea "github.com/charmbracelet/bubbletea"
 )
+
+type model struct {
+	stopwatch stopwatch.Model
+	keymap    keymap
+	help      help.Model
+	quitting  bool
+}
+
+type keymap struct {
+	start key.Binding
+	stop  key.Binding
+	reset key.Binding
+	quit  key.Binding
+}
+
+func (m model) Init() tea.Cmd {
+	return m.stopwatch.Init()
+}
+
+func (m model) View() string {
+	s := m.stopwatch.View() + "\n"
+	if !m.quitting {
+		s = "Elapsed: " + s
+		s += m.helpView()
+	}
+	return s
+}
+
+func (m model) helpView() string {
+	return "\n" + m.help.ShortHelpView([]key.Binding{
+		m.keymap.start,
+		m.keymap.stop,
+		m.keymap.reset,
+		m.keymap.quit,
+	})
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keymap.quit):
+			m.quitting = true
+			return m, tea.Quit
+		case key.Matches(msg, m.keymap.reset):
+			return m, m.stopwatch.Reset()
+		case key.Matches(msg, m.keymap.start, m.keymap.stop):
+			m.keymap.stop.SetEnabled(!m.stopwatch.Running())
+			m.keymap.start.SetEnabled(m.stopwatch.Running())
+			return m, m.stopwatch.Toggle()
+		}
+	}
+	var cmd tea.Cmd
+	m.stopwatch, cmd = m.stopwatch.Update(msg)
+	return m, cmd
+}
 
 func main() {
 	fmt.Println(getScramble())
-}
+	fmt.Println("Press space to start timer")
+	m := model{
+		stopwatch: stopwatch.NewWithInterval(time.Millisecond),
+		keymap: keymap{
+			start: key.NewBinding(
+				key.WithKeys("space"),
+				key.WithHelp("space", "start"),
+			),
+			stop: key.NewBinding(
+				key.WithKeys("space"),
+				key.WithHelp("space", "stop"),
+			),
+			reset: key.NewBinding(
+				key.WithKeys("r"),
+				key.WithHelp("r", "reset"),
+			),
+			quit: key.NewBinding(
+				key.WithKeys("ctrl+c", "q"),
+				key.WithHelp("q", "quit"),
+			),
+		},
+		help: help.NewModel(),
+	}
 
-const divisor = 2
+	m.keymap.start.SetEnabled(true)
 
-var (
-	columnStyle  = lipgloss.NewStyle().Padding(1, 2)
-	focusedStyle = lipgloss.NewStyle().Padding(1, 2)
-	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
-)
-
-type Time struct {
-	number   int
-	time     float32
-	scramble string
-	note     string
-}
-
-func (t Time) Number() int {
-	return t.number
-}
-
-func (t Time) Time() float32 {
-	return t.time
-}
-
-func (t Time) Note() string {
-	return t.note
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Oh no, it didn't work:", err)
+		os.Exit(1)
+	}
 }
